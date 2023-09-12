496aa4bd2c9edcb27d0b7b706eaf95d4b025cb04
Lucas Delobelle <lucas@ld-web.net>
2023-09-12 11:40:35 +0200

    :sparkles: add IndexController (homepage)

---
+++ src/Controller/IndexController.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class IndexController extends AbstractController
+{
+  #[Route('/', name: 'app_index')]
+  public function index(): Response
+  {
+    // Requête vers la BDD
+
+    return $this->render('index/index.html.twig', [
+      'name' => 'Dora Marsh',
+    ]);
+  }
+
+  #[Route('/coucou', name: 'coucou_damien')]
+  public function test(): Response
+  {
+    return new Response('Coucou Damien');
+  }
+}
