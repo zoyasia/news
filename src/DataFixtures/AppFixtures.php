236d80e8e13bd261e26c90d966f5f859c968db97
Lucas Delobelle <lucas@ld-web.net>
2023-09-14 09:58:05 +0200

    :sparkles: use Faker in Fixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,19 +5,23 @@ namespace App\DataFixtures;
 use App\Entity\Article;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
 
 class AppFixtures extends Fixture
 {
   private const NB_ARTICLES = 120;
   public function load(ObjectManager $manager): void
   {
+    $faker = Factory::create("zh_TW");
+
     for ($i = 1; $i <= self::NB_ARTICLES; $i++) {
       $article = new Article();
       $article
-        ->setTitle("Sandwich Ukraine 🇺🇦 " . $i)
-        ->setContent("library family mind whatever come many mission jungle home from middle airplane")
-        ->setDateCreated(new \DateTime())
-        ->setVisible(true);
+        // ->setTitle($faker->realTextBetween(20, 40))
+        ->setTitle($faker->words($faker->numberBetween(3, 7), true))
+        ->setContent($faker->realText(600))
+        ->setDateCreated($faker->dateTimeBetween('-2 years'))
+        ->setVisible($faker->boolean(80));
 
       $manager->persist($article);
     }
