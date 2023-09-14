ba8d3291931247ae7613fb13b12d188847aa5b40
Lucas Delobelle <lucas@ld-web.net>
2023-09-14 17:10:57 +0200

    :sparkles: add Article item page
    ParamConverter

---
+++ src/Controller/ArticleController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Article;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class ArticleController extends AbstractController
+{
+  #[Route('/article/{id}', name: 'app_article')]
+  public function index(Article $article): Response
+  {
+    return $this->render('article/index.html.twig', [
+      'article' => $article,
+    ]);
+  }
+}
