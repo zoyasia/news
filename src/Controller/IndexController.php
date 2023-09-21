956b8c61fdc171320db04d22a46d8946a68d04ef
Lucas Delobelle <lucas@ld-web.net>
2023-09-21 16:19:25 +0200

    :sparkles: HTTPClient, Scoped Client, POST request

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -9,11 +9,14 @@ use App\Repository\ArticleRepository;
 use App\Repository\NewsletterRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\FormError;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\HttpClient\Exception\ClientExceptionInterface;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class IndexController extends AbstractController
 {
@@ -37,7 +40,8 @@ class IndexController extends AbstractController
   public function newsletterSubscribe(
     Request $request,
     EntityManagerInterface $em,
-    NewsletterSubscribedConfirmation $notificationService
+    NewsletterSubscribedConfirmation $notificationService,
+    HttpClientInterface $spamChecker
   ): Response {
     // 1 - J'initialise une instance de mon entité
     $newsletterEmail = new Newsletter();
@@ -48,11 +52,29 @@ class IndexController extends AbstractController
     $form->handleRequest($request);
 
     if ($form->isSubmitted() && $form->isValid()) {
-      $em->persist($newsletterEmail);
-      $em->flush();
-      $notificationService->send($newsletterEmail);
-      // $this->addFlash("success", "Bravo ! Votre email a bien été enregistré !");
-      return $this->redirectToRoute("newsletter_sub_confirm");
+      $response = $spamChecker->request(
+        Request::METHOD_POST,
+        "/api/check",
+        [
+          'json' => ['email' => $newsletterEmail->getEmail()]
+        ]
+      );
+
+      try {
+        $data = $response->toArray();
+        $isSpam = $data['result'] === 'spam';
+
+        if ($isSpam) {
+          throw new \InvalidArgumentException();
+        }
+        $em->persist($newsletterEmail);
+        $em->flush();
+        $notificationService->send($newsletterEmail);
+        // $this->addFlash("success", "Bravo ! Votre email a bien été enregistré !");
+        return $this->redirectToRoute("newsletter_sub_confirm");
+      } catch (ClientExceptionInterface | \InvalidArgumentException $ex) {
+        $form->addError(new FormError("Une erreur est survenue lors de la vérification de l'email"));
+      }
     }
 
     return $this->renderForm('newsletter/subscribe.html.twig', [
